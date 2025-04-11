63dc4fbc41448210d70110a79d09a72049239abd
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2025-04-11 13:38:00 -0300

    onfigre initial projet

---
+++ Api.GRRInnovations.HealthMetrics/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace Api.GRRInnovations.HealthMetrics
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddHealthChecks();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+    }
+}
